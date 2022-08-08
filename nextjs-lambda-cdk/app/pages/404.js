d35e657ca232546b3727c093faefc768964183fc
Matthew de Anda <deandalm@gmail.com>
2022-08-08 01:50:59 +0000

    Updating sample application based on Next.js walk through.

---
+++ nextjs-lambda-cdk/app/pages/404.js
@@ -0,0 +1,11 @@
+import ErrorLayout from '../components/error-layout';
+
+const Custom404 = () => {
+  return (
+    <ErrorLayout>
+      <h1>404 - Page Not Found</h1>
+    </ErrorLayout>
+  );
+}
+
+export default Custom404;
--- nextjs-lambda-cdk/app/pages/api/hello.js
+++ nextjs-lambda-cdk/app/pages/api/hello.js
@@ -1,5 +1,12 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+const handler = (req, res) => {
+  res.status(200).json({
+    text: 'Hello',
+    region: process.env.AWS_REGION,
+    runtime: process.env.AWS_EXECUTION_ENV,
+    memory: process.env.AWS_LAMBDA_FUNCTION_MEMORY_SIZE,
+    timezone: process.env.TZ,
+    date: Date(),
+  })
 }
+
+export default handler;
